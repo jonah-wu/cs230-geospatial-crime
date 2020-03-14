6ac742427b559b52a2c2185d9476b42b560a90cb
Jonah Wu <jonahwu@Jonahs-MacBook-Pro.local>
2020-03-14 03:06:03 -0700

    Binning bug fixed

---
+++ scripts/b2f_satstreet.py
@@ -0,0 +1,147 @@
+import os
+
+# path to image folder, get all filenames on this folder
+# and store it in the onlyfiles list
+
+
+def bin_files(bin1, bin2, bin3, sat_path, street_path):
+    satellite_path = sat_path
+    sat19_files = [f for f in os.listdir(satellite_path) if os.path.isfile(os.path.join(satellite_path, f))]
+    #print(onlyfiles)
+    #print(bin1)
+    streetview_path = street_path 
+    street_files = [f for f in os.listdir(streetview_path) if os.path.isfile(os.path.join(streetview_path, f))]
+    # print(bin1)
+    # print(bin2)
+    # print(bin3)
+    
+    sat19_copy_path = "../data/satellite_imgs/sat19_imgs_cpy"
+    sat19_copy_files = [f for f in os.listdir(sat19_copy_path) if os.path.isfile(os.path.join(sat19_copy_path, f))]
+    
+    
+    sat19_lis1files = []
+    sat19_lis2files = []
+    sat19_lis3files = []
+
+    lis1files = []
+    lis2files = []
+    lis3files = []
+
+    print(street_files)
+    print(sat19_files)
+
+    for j in bin1:
+        if j + '_sat_19.jpg' in sat19_copy_files and j + '.jpg' in street_files:
+            lis1files.append(j + '.jpg')
+            sat19_lis1files.append(j + '_sat_19.jpg')
+
+    for j in bin2:
+        if j + '_sat_19.jpg' in sat19_copy_files and j + '.jpg' in street_files:
+            lis2files.append(j + '.jpg')
+            sat19_lis2files.append(j + '_sat_19.jpg')
+
+    for j in bin3:
+        if j + '_sat_19.jpg' in sat19_copy_files and j + '.jpg' in street_files:
+            lis3files.append(j + '.jpg')
+            sat19_lis3files.append(j + '_sat_19.jpg')
+
+
+            #for j in bin1:
+        #if j + '.jpg' in onlyfiles:
+#lis1files.append(j+'.jpg')
+#print(len(lis1files))
+
+
+    # create three sub folders in mypath folder
+    subfolder1 = os.path.join(satellite_path, "bin1")
+    subfolder2 = os.path.join(satellite_path, "bin2")
+    subfolder3 = os.path.join(satellite_path, "bin3")
+
+    # check if they already exits to prevent error
+    if not os.path.exists(subfolder1):
+        os.makedirs(subfolder1)
+
+    if not os.path.exists(subfolder2):
+        os.makedirs(subfolder2)
+
+    if not os.path.exists(subfolder3):
+        os.makedirs(subfolder3)
+
+    # move files to their respective sub folders
+    for i in sat19_lis1files:
+        #print(i)
+        source = os.path.join(satellite_path, i)
+        destination = os.path.join(subfolder1, i)
+        if os.path.isfile(source):
+            os.rename(source, destination)
+
+    for i in sat19_lis2files:
+        #print(i)
+        source = os.path.join(satellite_path, i)
+        destination = os.path.join(subfolder2, i)
+        if os.path.isfile(source):
+            os.rename(source, destination)
+
+    for i in sat19_lis3files:
+        #print(i)
+        source = os.path.join(satellite_path, i)
+        destination = os.path.join(subfolder3, i)
+        if os.path.isfile(source):
+            os.rename(source, destination)
+
+    #print(onlyfiles)
+    #print(bin1)
+
+    # print(bin1)
+    # print(bin2)
+    # print(bin3)
+    # create three sub folders in mypath folder
+    street_subfolder1 = os.path.join(streetview_path, "bin1")
+    street_subfolder2 = os.path.join(streetview_path, "bin2")
+    street_subfolder3 = os.path.join(streetview_path, "bin3")
+
+    # check if they already exits to prevent error
+    if not os.path.exists(street_subfolder1):
+        os.makedirs(street_subfolder1)
+
+    if not os.path.exists(street_subfolder2):
+        os.makedirs(street_subfolder2)
+
+    if not os.path.exists(street_subfolder3):
+        os.makedirs(street_subfolder3)
+
+    # move files to their respective sub folders
+    print("This is lis1files")
+    print(len(lis1files))
+    for i in lis1files:
+        #print(i)
+        source = os.path.join(streetview_path, i)
+        destination = os.path.join(street_subfolder1, i)
+        if os.path.isfile(source):
+            os.rename(source, destination)
+    print("This is lis2files")
+    print(len(lis2files))
+    for i in lis2files:
+        #print(i)
+        source = os.path.join(streetview_path, i)
+        destination = os.path.join(street_subfolder2, i)
+        if os.path.isfile(source):
+            os.rename(source, destination)
+    print("This is lis3files")
+    print(len(lis3files))
+    for i in lis3files:
+        #print(i)
+        source = os.path.join(streetview_path, i)
+        destination = os.path.join(street_subfolder3, i)
+        if os.path.isfile(source):
+            os.rename(source, destination)
+
+    print(len(lis1files + lis2files + lis3files))
+    print(len(sat19_lis1files + sat19_lis2files + sat19_lis3files))
+
+
+
+
+
+
+
--- scripts/indx_to_folders.py
+++ scripts/indx_to_folders.py
@@ -1,6 +1,6 @@
 import pickle
 import bin_to_folder as b2f
-
+import b2f_satstreet 
 
 b0_pkl = open("../data/b0_list.pickle","rb")
 b0 = pickle.load(b0_pkl)
@@ -11,8 +11,10 @@ b1 = pickle.load(b1_pkl)
 b2_pkl = open("../data/b2_list.pickle","rb")
 b2 = pickle.load(b2_pkl)
 
-data_path = "../data/streetview_imgs"
-b2f.bin_files(b0, b1, b2, data_path)
+street_path = "../data/streetview_imgs"
+#b2f.bin_files(b0, b1, b2, data_path)
 
-data_path = "../data/satellite_imgs"
-b2f.bin_files(b0, b1, b2, data_path)
+sat_path = "../data/satellite_imgs/sat19_imgs"
+#b2f.bin_files(b0, b1, b2, data_path)
+
+b2f_satstreet.bin_files(b0,b1,b2, sat_path, street_path)
